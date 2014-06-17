419c06bb1500653cc8d6113b42f11cd364bc509c
Kuba Orlik <groovy354@gmail.com>
2014-06-17 11:37:24 +0200

    tutorial do referencji

---
+++ Tut 07 World in Motion/programs.h
@@ -0,0 +1,52 @@
+struct ProgramData
+{
+	GLuint theProgram;
+	GLuint modelToWorldMatrixUnif;
+	GLuint worldToCameraMatrixUnif;
+	GLuint cameraToClipMatrixUnif;
+	GLuint baseColorUnif;
+};
+
+float g_fzNear = 1.0f;
+float g_fzFar = 1000.0f;
+
+ProgramData UniformColor;
+ProgramData ObjectColor;
+ProgramData UniformColorTint;
+
+ProgramData LoadProgram(const std::string &strVertexShader, const std::string &strFragmentShader)
+{
+	std::vector<GLuint> shaderList;
+
+	shaderList.push_back(Framework::LoadShader(GL_VERTEX_SHADER, strVertexShader));
+	shaderList.push_back(Framework::LoadShader(GL_FRAGMENT_SHADER, strFragmentShader));
+
+	ProgramData data;
+	data.theProgram = Framework::CreateProgram(shaderList);
+	data.modelToWorldMatrixUnif = glGetUniformLocation(data.theProgram, "modelToWorldMatrix");
+	data.worldToCameraMatrixUnif = glGetUniformLocation(data.theProgram, "worldToCameraMatrix");
+	data.cameraToClipMatrixUnif = glGetUniformLocation(data.theProgram, "cameraToClipMatrix");
+	data.baseColorUnif = glGetUniformLocation(data.theProgram, "baseColor");
+
+	return data;
+}
+
+void InitializeProgram()
+{
+	UniformColor = LoadProgram("PosOnlyWorldTransform.vert", "ColorUniform.frag");
+	ObjectColor = LoadProgram("PosColorWorldTransform.vert", "ColorPassthrough.frag");
+	UniformColorTint = LoadProgram("PosColorWorldTransform.vert", "ColorMultUniform.frag");
+}
+
+void setupShaderData(int w, int h){
+	glutil::MatrixStack persMatrix;
+	persMatrix.Perspective(45.0f, (w / (float)h), g_fzNear, g_fzFar);
+
+	glUseProgram(UniformColor.theProgram);
+	glUniformMatrix4fv(UniformColor.cameraToClipMatrixUnif, 1, GL_FALSE, glm::value_ptr(persMatrix.Top()));
+	glUseProgram(ObjectColor.theProgram);
+	glUniformMatrix4fv(ObjectColor.cameraToClipMatrixUnif, 1, GL_FALSE, glm::value_ptr(persMatrix.Top()));
+	glUseProgram(UniformColorTint.theProgram);
+	glUniformMatrix4fv(UniformColorTint.cameraToClipMatrixUnif, 1, GL_FALSE, glm::value_ptr(persMatrix.Top()));
+	glUseProgram(0);
+}
